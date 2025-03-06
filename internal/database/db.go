612a34143cc56cd9476f93d2585adda2b1d2aee8
Fábio <fabio.oliveira@poscontrole.com.br>
2025-03-05 22:50:29 -0300

    🐛 fix(database): add unique constraint to email field
    - ensure email field uniqueness in companies table to avoid duplicate entries

--- internal/database/db.go
+++ internal/database/db.go
@@ -20,7 +20,7 @@ func initDB() {
 	CREATE TABLE IF NOT EXISTS companies (
 		id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
 		name TEXT NOT NULL,
-		email TEXT NOT NULL,
+		email TEXT NOT NULL UNIQUE,
 		password TEXT NOT NULL
 	)`
 
