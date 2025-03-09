4fbda5fa2b418d665da4f380a46af3c3bd7e4632
Fábio <fabio.oliveira@poscontrole.com.br>
2025-03-09 20:13:29 -0300

    ✨ feat(database): add database initialization functionality
    - create database package with initDB function to initialize SQLite database
    - define Users table schema with id, name, email, password, and user_type columns

---
+++ internal/infra/database/db.go
@@ -0,0 +1,36 @@
+package database
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+var DB *sql.DB
+
+func initDB() {
+	var err error
+	DB, err = sql.Open("sqlite3", "./app.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sqlStmt := `
+	CREATE TABLE IF NOT EXISTS Users (
+		id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+		name TEXT NOT NULL,
+		email TEXT NOT NULL UNIQUE,
+		password TEXT NOT NULL,
+		user_type INTEGER NOT NULL
+	)`
+
+	_, err = DB.Exec(sqlStmt)
+	if err != nil {
+		log.Fatalf("Error creating table: %q: %s\n", err, sqlStmt)
+	}
+}
+
+func init() {
+	initDB()
+}
