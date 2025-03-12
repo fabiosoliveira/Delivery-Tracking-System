95cc1c171f32352c945e359b33b7fb59d1e1d414
Fábio <fabio.oliveira@poscontrole.com.br>
2025-03-11 23:03:22 -0300

    🔧 chore(domain): remove redundant Company struct
    - delete Company struct as it is not being used
    - refactor NewCompany function to return User pointer directly

--- internal/domain/company.go
+++ internal/domain/company.go
@@ -1,9 +1,5 @@
 package domain
 
-type Company struct {
-	User
-}
-
 func NewCompany(name string, email string, password string) (*User, error) {
 	user, err := NewUser(name, email, password, UserTypeCompany)
 	if err != nil {
