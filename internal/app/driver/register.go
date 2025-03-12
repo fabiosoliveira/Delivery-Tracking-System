79f668b7dae22fdf25e51b4e17064afc32cbe933
Fábio <fabio.oliveira@poscontrole.com.br>
2025-03-11 23:00:11 -0300

    🚀 feat(driver): implement driver registration feature
    - create Register struct with UserRepository dependency
    - add RegisterInput struct for driver registration data
    - implement Execute method to register a new driver
    - handle existing driver error and save new driver to repository

---
+++ internal/app/driver/register.go
@@ -0,0 +1,46 @@
+package auth
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/fabiosoliveira/Delivery-Tracking-System/internal/domain"
+)
+
+type Register struct {
+	userRepository domain.UserRepository
+}
+
+func NewRegister(repository domain.UserRepository) *Register {
+	return &Register{
+		userRepository: repository,
+	}
+}
+
+func (r *Register) Execute(inut *RegisterInput) error {
+	driver, err := r.userRepository.FindByEmail(&inut.Email)
+	if err != nil {
+		return fmt.Errorf("error register driver: %w", err)
+	}
+
+	if driver != nil {
+		return errors.New("error register driver: driver already exists")
+	}
+
+	driver, err = domain.NewUser(inut.Name, inut.Email, inut.Password, domain.UserTypeDriver)
+	if err != nil {
+		return fmt.Errorf("error register driver: %w", err)
+	}
+
+	err = r.userRepository.Save(driver)
+	if err != nil {
+		return fmt.Errorf("error register driver: %w", err)
+	}
+	return nil
+}
+
+type RegisterInput struct {
+	Name     string
+	Email    string
+	Password string
+}
