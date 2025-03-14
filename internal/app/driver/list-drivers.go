bfe1e5d45bf07dd9b901d7a6acb4b93617d9eeaf
Fábio <fabio.oliveira@poscontrole.com.br>
2025-03-14 00:39:02 -0300

    🚀 feat(driver): add list drivers feature
    - implement ListDrivers struct to handle driver listing functionality
    - define Execute method to fetch and format drivers data
    - introduce ListDriversOutput struct for driver information representation

---
+++ internal/app/driver/list-drivers.go
@@ -0,0 +1,41 @@
+package driver
+
+import (
+	"fmt"
+
+	"github.com/fabiosoliveira/Delivery-Tracking-System/internal/domain"
+)
+
+type ListDrivers struct {
+	userRepository domain.UserRepository
+}
+
+func NewListDrivers(repository domain.UserRepository) *ListDrivers {
+	return &ListDrivers{
+		userRepository: repository,
+	}
+}
+
+func (r *ListDrivers) Execute(userId int) ([]ListDriversOutput, error) {
+	drivers, err := r.userRepository.ListDriversByCompanyId(userId)
+	if err != nil {
+		return nil, fmt.Errorf("error listing drivers: %w", err)
+	}
+
+	if len(drivers) == 0 {
+		return nil, fmt.Errorf("error listing drivers: no drivers found")
+	}
+
+	var driversOutput []ListDriversOutput
+	for _, driver := range drivers {
+		driversOutput = append(driversOutput, ListDriversOutput{int(driver.ID()), driver.Name(), driver.Email()})
+	}
+
+	return driversOutput, nil
+}
+
+type ListDriversOutput struct {
+	ID    int
+	Name  string
+	Email string
+}
