b059349d03db6863fee6123e68491b2fb0a37502
Fábio <fabio.oliveira@poscontrole.com.br>
2025-03-14 00:38:40 -0300

    🔧 chore(domain): refactor driver struct and functions
    - update NewDriver function to include companyId parameter
    - adjust NewDriver to return a Driver struct instead of User
    - add RestoreDriver function to create a Driver from existing data
    - implement UserType and CompanyId methods in Driver struct

--- internal/domain/driver.go
+++ internal/domain/driver.go
@@ -1,10 +1,37 @@
 package domain
 
-func NewDriver(name string, email string, password string) (*User, error) {
-	user, err := NewUser(name, email, password, UserTypeDriver)
+type Driver struct {
+	User
+	companyId uint
+}
+
+func NewDriver(name string, email string, password string, companyId uint) (User, error) {
+
+	user, err := newUser(name, email, password)
 	if err != nil {
 		return nil, err
 	}
 
-	return user, nil
+	driver := &Driver{
+		User:      user,
+		companyId: companyId,
+	}
+
+	return driver, nil
+}
+
+func RestoreDriver(id int, name string, _email string, passwordHash string, companyId int) User {
+	user := restoreUser(id, name, _email, passwordHash)
+	return &Driver{
+		User:      user,
+		companyId: uint(companyId),
+	}
+}
+
+func (d *Driver) UserType() userType {
+	return UserTypeDriver
+}
+
+func (d *Driver) CompanyId() uint {
+	return d.companyId
 }
