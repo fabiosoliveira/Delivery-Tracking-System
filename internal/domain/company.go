541842f27ba65e4a9dee74a1f5dc96c883f3747b
Fábio <fabio.oliveira@poscontrole.com.br>
2025-03-14 00:37:24 -0300

    ♻️ refactor(company): improve company struct and functions
    - change NewCompany signature to return Company instead of pointer to User
    - update NewCompany to initialize Company struct and assign User field
    - add RestoreCompany function to create Company instance from existing data
    - implement UserType method to return UserTypeCompany for Company instances

--- internal/domain/company.go
+++ internal/domain/company.go
@@ -1,10 +1,28 @@
 package domain
 
-func NewCompany(name string, email string, password string) (*User, error) {
-	user, err := NewUser(name, email, password, UserTypeCompany)
+type Company struct {
+	User
+}
+
+func NewCompany(name string, email string, password string) (User, error) {
+	company := &Company{}
+
+	user, err := newUser(name, email, password)
 	if err != nil {
 		return nil, err
 	}
 
-	return user, nil
+	company.User = user
+
+	return company, nil
+}
+
+func RestoreCompany(id int, name string, _email string, passwordHash string) User {
+	return &Company{
+		User: restoreUser(id, name, _email, passwordHash),
+	}
+}
+
+func (c *Company) UserType() userType {
+	return UserTypeCompany
 }
