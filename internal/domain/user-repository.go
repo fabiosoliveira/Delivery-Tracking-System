f0f2c04c66f2cf7afd3c9acbe6208f04b4db1952
Fábio <fabio.oliveira@poscontrole.com.br>
2025-06-04 19:39:29 -0300

    ✨ feat(domain): add FindById method to UserRepository interface
    - introduce FindById method to retrieve a user by ID for better data access flexibility

--- internal/domain/user-repository.go
+++ internal/domain/user-repository.go
@@ -3,5 +3,6 @@ package domain
 type UserRepository interface {
 	Save(user User) error
 	FindByEmail(email *string) (User, error)
+	FindById(id uint) (User, error)
 	ListDriversByCompanyId(id int) ([]User, error)
 }
