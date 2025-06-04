743815591caa153782323fe9014ea22be392dd59
Fábio <fabio.oliveira@poscontrole.com.br>
2025-06-04 19:39:42 -0300

    🚀 feat(domain): implement delivery domain logic
    - define Location and Delivery structs with necessary fields
    - implement NewDelivery and RestoreDelivery functions for creating new deliveries
    - add methods for getting delivery details like Id, Status, Company_id, Driver_id, Locations, Recipient, Address
    - include method AddLocation to add new location to a delivery

---
+++ internal/domain/delivery.go
@@ -0,0 +1,79 @@
+package domain
+
+type Location struct {
+	id        uint
+	latitude  float64
+	longitude float64
+	timestamp int64
+}
+
+type Delivery struct {
+	id         uint
+	status     statusDelivery
+	company_id uint
+	driver_id  uint
+	locations  []Location
+	recipient  string
+	address    string
+}
+
+func NewDelivery(company_id uint, driver_id uint, recipient string, address string) *Delivery {
+	return &Delivery{
+		status:     StatusPendente,
+		company_id: company_id,
+		driver_id:  driver_id,
+		locations:  []Location{},
+		recipient:  recipient,
+		address:    address,
+	}
+}
+
+func RestoreDelivery(id uint, status uint8, company_id uint, driver_id uint, recipient string, address string) *Delivery {
+	return &Delivery{
+		id:         id,
+		status:     statusDelivery(status),
+		company_id: company_id,
+		driver_id:  driver_id,
+		locations:  []Location{},
+		recipient:  recipient,
+		address:    address,
+	}
+}
+
+func (d *Delivery) Id() uint {
+	return d.id
+}
+
+func (d *Delivery) Status() statusDelivery {
+	return d.status
+}
+
+func (d *Delivery) Company_id() uint {
+	return d.company_id
+}
+
+func (d *Delivery) Driver_id() uint {
+	return d.driver_id
+}
+
+func (d *Delivery) Locations() []Location {
+	return d.locations
+}
+
+func (d *Delivery) Recipient() string {
+	return d.recipient
+}
+
+func (d *Delivery) Address() string {
+	return d.address
+}
+
+func (d *Delivery) AddLocation(id uint, latitude float64, longitude float64, timestamp int64) {
+	location := Location{
+		id:        id,
+		latitude:  latitude,
+		longitude: longitude,
+		timestamp: timestamp,
+	}
+	d.locations = append(d.locations, location)
+}
