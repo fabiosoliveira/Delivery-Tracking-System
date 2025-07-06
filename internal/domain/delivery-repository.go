1141b406c1576c97928f149776f7a94207cbde0f
Fábio <fabio.oliveira@poscontrole.com.br>
2025-07-06 19:54:53 -0300

    ✨ feat(domain): add new methods to DeliveryRepository
    - add ListDeliveryByDriverId method to retrieve deliveries by driver ID
    - add FindById method to find delivery by ID
    - add UpdateLocation method to update delivery location

--- internal/domain/delivery-repository.go
+++ internal/domain/delivery-repository.go
@@ -3,4 +3,7 @@ package domain
 type DeliveryRepository interface {
 	Save(delivery *Delivery) error
 	ListDeliveryByCompanyId(id int) ([]Delivery, error)
+	ListDeliveryByDriverId(driverId int) ([]Delivery, error)
+	FindById(deliveryId uint) (*Delivery, error)
+	UpdateLocation(location *Location, deliveryId uint) error
 }
