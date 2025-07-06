e6b05f48a1982b29be2919bcf182a9040f15950a
Fábio <fabio.oliveira@poscontrole.com.br>
2025-07-06 19:55:24 -0300

    🆕 feat(ws): implement WebSocket server handling
    - create WebSocket server package with server-side logic for handling connections
    - define struct for GPS data with delivery ID, latitude, and longitude
    - implement connection upgrade and handling function for receiving GPS data
    - add function to handle delivery-related requests and responses
    - include function to manage user login requests and responses
    - implement error response function for consistent error handling

---
+++ internal/app/ws/ws.go
@@ -0,0 +1,166 @@
+package ws
+
+import (
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"io"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/fabiosoliveira/Delivery-Tracking-System/internal/app/auth"
+	"github.com/fabiosoliveira/Delivery-Tracking-System/internal/app/delivery"
+	"github.com/fabiosoliveira/Delivery-Tracking-System/internal/cookies"
+	"github.com/fabiosoliveira/Delivery-Tracking-System/internal/infra/database"
+	"github.com/gorilla/websocket"
+)
+
+type GPSData struct {
+	Delivery  int64   `json:"delivery_id"`
+	Latitude  float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
+}
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+func HandleConnection(w http.ResponseWriter, r *http.Request) {
+	deliveryRepository := database.NewDeliveryRepositorySqlite(database.DB)
+	sendLocation := delivery.NewSendLocation(deliveryRepository)
+
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println("Erro ao atualizar conexão:", err)
+		return
+	}
+	defer conn.Close()
+
+	log.Println("Cliente conectado")
+
+	for {
+		var gpsData GPSData
+
+		err := conn.ReadJSON(&gpsData)
+		if err != nil {
+			log.Println("Erro ao ler mensagem:", err)
+			break
+		}
+
+		log.Printf("Coordenadas recebidas: Delivery: %d, Latitude: %f, Longitude: %f\n", gpsData.Delivery, gpsData.Latitude, gpsData.Longitude)
+
+		err = sendLocation.Execute(&delivery.SendLocationInput{
+			Delivery:  gpsData.Delivery,
+			Latitude:  gpsData.Latitude,
+			Longitude: gpsData.Longitude,
+		})
+		if err != nil {
+			log.Println("Erro ao enviar localização:", err)
+			break
+		}
+	}
+}
+
+func AppDelivery(w http.ResponseWriter, r *http.Request) {
+	homeTemplate := template.Must(template.ParseFiles("template/app-delivery.gohtml"))
+
+	// homeTemplate.Execute(w, "wss://"+r.Host+"/ws")
+	homeTemplate.Execute(w, "wss://hm3c050c-8080.brs.devtunnels.ms/ws")
+}
+
+func LoginAppDelivery(w http.ResponseWriter, r *http.Request) {
+	log.Println("POST /LoginAppDelivery")
+
+	userRepository := database.NewUserRepositorySqlite(database.DB)
+	signIn := auth.NewSignIn(userRepository)
+
+	deliveryRepository := database.NewDeliveryRepositorySqlite(database.DB)
+	listDelivery := NewListDelivery(deliveryRepository, userRepository)
+
+	jsonData, err := io.ReadAll(r.Body)
+	if err != nil {
+		fmt.Println("Error reading response:", err)
+		return
+	}
+
+	var userData struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	err = json.Unmarshal(jsonData, &userData)
+	if err != nil {
+		fmt.Println("Error unmarshaling:", err)
+		return
+	}
+	fmt.Println("Decoded User:", userData)
+
+	signInInput := &auth.SignInInput{
+		Email:    userData.Email,
+		Password: userData.Password,
+	}
+
+	output, err := signIn.Execute(signInInput)
+	if err != nil {
+		respondWithError(w, "Erro ao efetuar login: "+err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	fmt.Println("Output:", output)
+
+	if *output.UserType != "driver" {
+		respondWithError(w, "Erro ao efetuar login: usuário não é um motorista", http.StatusBadRequest)
+		return
+	}
+
+	driverId, err := strconv.Atoi(*output.UserId)
+	if err != nil {
+		respondWithError(w, "Erro ao efetuar login: "+err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	deliveries, err := listDelivery.Execute(driverId)
+	if err != nil {
+		respondWithError(w, "Erro ao efetuar login: "+err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	value := map[string]string{
+		"UserId":   *output.UserId,
+		"UserType": *output.UserType,
+	}
+
+	if encoded, err := cookies.S.Encode("userCookieDriver", value); err == nil {
+		cookie := &http.Cookie{
+			Name:     "userCookieDriver",
+			Value:    encoded,
+			Path:     "/",
+			MaxAge:   3600,
+			HttpOnly: true,
+			Secure:   true,
+			SameSite: http.SameSiteLaxMode,
+		}
+		http.SetCookie(w, cookie)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(deliveries)
+}
+
+func respondWithError(w http.ResponseWriter, message string, statusCode int) {
+	response := struct {
+		Message    string `json:"message"`
+		StatusCode int    `json:"statusCode"`
+	}{
+		Message:    message,
+		StatusCode: statusCode,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	json.NewEncoder(w).Encode(response)
+}
