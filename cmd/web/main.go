04c90b887a21264abf1775be79ae9cf12832d9b9
Fábio <fabio.oliveira@poscontrole.com.br>
2025-07-20 16:25:33 -0300

    🔥 refactor(main): optimize main module setup
    - remove unused imports and variables
    - clean up commented out code for better readability
    
    ♻️ refactor(driver): optimize driver module setup
    
    - remove unnecessary code for listing and registering drivers
    
    ⚡️ perf(email): remove email validation functionality
    
    - delete unnecessary email validation logic from the auth package
    
    🔧 chore: remove unused domain files
    
    - delete unused domain files related to company, driver, email, error, password, user-repository, user-type, and user

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,18 +4,15 @@ import (
 	"context"
 	"fmt"
 	"html/template"
-	"log"
 	"net/http"
 	"os"
 	"os/signal"
-	"strconv"
 	"time"
 
 	"github.com/fabiosoliveira/Delivery-Tracking-System/internal/app/auth"
 	"github.com/fabiosoliveira/Delivery-Tracking-System/internal/app/delivery"
 	"github.com/fabiosoliveira/Delivery-Tracking-System/internal/app/driver"
 	"github.com/fabiosoliveira/Delivery-Tracking-System/internal/app/ws"
-	"github.com/fabiosoliveira/Delivery-Tracking-System/internal/cookies"
 	"github.com/fabiosoliveira/Delivery-Tracking-System/internal/infra/database"
 )
 
@@ -28,106 +25,12 @@ fabio@gmail.com
 func main() {
 
 	db := database.DB
-	userRepository := database.NewUserRepositorySqlite(db)
-	// signUp := auth.NewSignUp(userRepository)
-	// signIn := auth.NewSignIn(userRepository)
-	listDrivers := driver.NewListDrivers(userRepository)
-	registerDriver := driver.NewRegister(userRepository)
-
-	// deliveryRepository := database.NewDeliveryRepositorySqlite(db)
-	// createDelivery := delivery.NewCreateDelivery(deliveryRepository, userRepository)
-	// listDelivery := delivery.NewListDelivery(deliveryRepository, userRepository)
-	// getDeliveryHistory := delivery.NewGetDeliveryHistory(deliveryRepository)
-
-	// companyDao := database.NewCompanyDataAccessObject(db)
 
 	mux := http.NewServeMux()
 
 	auth.Module(mux, db)
 	delivery.Module(mux, db)
-
-	mux.HandleFunc("GET /drivers", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("GET /drivers")
-		cookie, err := r.Cookie("userCookie")
-		if err != nil {
-			log.Println("GET /drivers: cookie not found")
-			http.Redirect(w, r, "/auth/signin", http.StatusSeeOther)
-			return
-		}
-
-		value := make(map[string]string)
-
-		err = cookies.S.Decode("userCookie", cookie.Value, &value)
-		if err != nil {
-			log.Println("GET /drivers: error decoding cookie", err)
-			http.Redirect(w, r, "/auth/signin", http.StatusSeeOther)
-			return
-		}
-
-		id, err := strconv.Atoi(value["UserId"])
-		if err != nil {
-			log.Println("GET /drivers: error converting UserId to int", err)
-			http.Redirect(w, r, "/auth/signin", http.StatusSeeOther)
-			return
-		}
-
-		drivers, err := listDrivers.Execute(id)
-		if err != nil {
-			TrowError(err, w, r)
-			return
-
-		}
-		tpl := template.Must(template.ParseFiles("template/layout.gohtml", "template/drivers.gohtml", "template/drivers-row.gohtml"))
-
-		tpl.ExecuteTemplate(w, "layout", drivers)
-	})
-
-	mux.HandleFunc("POST /drivers", func(w http.ResponseWriter, r *http.Request) {
-		cookie, err := r.Cookie("userCookie")
-		if err != nil {
-			http.Redirect(w, r, "/auth/signin", http.StatusSeeOther)
-			return
-		}
-
-		value := make(map[string]string)
-
-		err = cookies.S.Decode("userCookie", cookie.Value, &value)
-		if err != nil {
-			http.Redirect(w, r, "/auth/signin", http.StatusSeeOther)
-			return
-		}
-
-		id, err := strconv.Atoi(value["UserId"])
-		if err != nil {
-			http.Redirect(w, r, "/auth/signin", http.StatusSeeOther)
-			return
-		}
-
-		err = r.ParseForm()
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-
-		name := r.FormValue("name")
-		email := r.FormValue("email")
-		password := r.FormValue("password")
-
-		register := &driver.RegisterInput{
-			Name:      name,
-			Email:     email,
-			Password:  password,
-			CompanyId: uint(id),
-		}
-
-		err = registerDriver.Execute(register)
-		if err != nil {
-			TrowError(err, w, r)
-			return
-		}
-
-		http.Redirect(w, r, "/drivers", http.StatusSeeOther)
-	})
+	driver.Module(mux, db)
 
 	mux.HandleFunc("GET /app-delivery", ws.AppDelivery)
 	mux.HandleFunc("POST /app-delivery/login", ws.LoginAppDelivery)
--- internal/domain/user-repository.go
+++
@@ -1,8 +0,0 @@
-package domain
-
-type UserRepository interface {
-	Save(user User) error
-	FindByEmail(email *string) (User, error)
-	FindById(id uint) (User, error)
-	ListDriversByCompanyId(id int) ([]User, error)
-}
