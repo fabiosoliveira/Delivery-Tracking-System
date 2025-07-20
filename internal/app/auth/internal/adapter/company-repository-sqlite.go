d998ea0c0f5f715682f596f0a37ea9f53d179f8d
Fábio <fabio.oliveira@poscontrole.com.br>
2025-07-20 12:51:27 -0300

    🔧 chore(adapter): remove unused commented-out code
    - Remove commented-out code for findById and listDriversByCompanyId functions to clean up the file.

--- internal/app/auth/internal/adapter/company-repository-sqlite.go
+++ internal/app/auth/internal/adapter/company-repository-sqlite.go
@@ -26,30 +26,6 @@ func (c CompanyRepositorySqlite) Save(company *domain.Company) error {
 	return nil
 }
 
-// func (c CompanyRepositorySqlite) findById(userId uint) (domain.Company, error) {
-// 	row := c.db.QueryRow("SELECT * FROM Users WHERE id = ?", userId)
-
-// 	var id int64
-// 	var companyId any
-// 	var name, _email, passwordHash string
-
-// 	err := row.Scan(&id, &name, &_email, &passwordHash, &companyId)
-// 	if err != nil {
-// 		if err == sql.ErrNoRows {
-// 			return nil, nil
-// 		}
-// 		return nil, fmt.Errorf("error finding user: %w", err)
-// 	}
-
-// 	if companyId != nil {
-// 		user := RestoreDriver(int(id), name, _email, passwordHash, int(companyId.(int64)))
-// 		return user, nil
-// 	}
-
-// 	user := RestoreCompany(int(id), name, _email, passwordHash)
-// 	return user, nil
-// }
-
 func (c CompanyRepositorySqlite) FindByEmail(email *string) (*domain.Company, error) {
 	row := c.db.QueryRow("SELECT * FROM Users WHERE email = ? AND company_id IS NULL", email)
 
@@ -68,26 +44,3 @@ func (c CompanyRepositorySqlite) FindByEmail(email *string) (*domain.Company, er
 	company := domain.RestoreCompany(int(id), name, _email, passwordHash)
 	return company, nil
 }
-
-// func (c CompanyRepositorySqlite) listDriversByCompanyId(id int) ([]domain.Company, error) {
-// 	rows, err := ur.db.Query("SELECT * FROM Users WHERE company_id = ?", id)
-// 	if err != nil {
-// 		return nil, fmt.Errorf("error listing drivers: %w", err)
-// 	}
-// 	defer rows.Close()
-
-// 	var drivers []domain.Company
-// 	for rows.Next() {
-// 		var id, companyId int
-// 		var name, _email, passwordHash string
-// 		if err := rows.Scan(&id, &name, &_email, &passwordHash, &companyId); err != nil {
-// 			return nil, fmt.Errorf("error listing drivers: %w", err)
-// 		}
-
-// 		driver := domain.RestoreDriver(id, name, _email, passwordHash, companyId)
-
-// 		drivers = append(drivers, driver)
-// 	}
-
-// 	return drivers, nil
-// }
